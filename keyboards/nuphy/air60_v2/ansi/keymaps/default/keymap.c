6ad84f94b2a92d8caa840d2b75c5bd7bf9b41f2b
Hrishikesh Sawant <hrishikesh.sawant322@gmail.com>
2025-01-02 17:03:01 +0900

    set layer 3 for gaming

--- keyboards/nuphy/air60_v2/ansi/keymaps/default/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/default/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
 	TD(TD_ESC), 	KC_1,           KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,        KC_EQL, 	KC_BSPC,
 	KC_TAB,             KC_Q,           KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,        KC_RBRC, 	KC_BSLS,
-	KC_CAPS,            LT(2, KC_A),   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	LT(3, KC_QUOT), 	        KC_ENT,
+	KC_CAPS,            LT(2, KC_A),   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	LT(4, KC_QUOT), 	        KC_ENT,
 	KC_LSFT,            KC_Z,           KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,        KC_UP,		KC_DEL,
 	KC_LCTL,            KC_LGUI,        KC_LALT,										KC_SPC, 							KC_RALT,	MO(1),   	KC_LEFT,        KC_DOWN,    KC_RGHT),
 
@@ -32,17 +32,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______,	_______,	_______,										_______, 							_______,	_______,   	_______,	_______,    _______),
 
 [3] = LAYOUT(
+	TD_ESC, 	KC_1,       KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,        KC_EQL, 	KC_BSPC,
+	KC_TAB,     KC_Q,       KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,        KC_RBRC, 	KC_BSLS,
+	KC_CAPS,    KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	    KC_ENT,
+	KC_LSFT,    KC_Z,       KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,        KC_UP,		KC_DEL,
+	KC_LCTL,    KC_LGUI,    KC_LALT,										KC_SPC, 							KC_RALT,	MO(1),   	KC_LEFT,        KC_DOWN,    KC_RGHT),
+
+[4] = LAYOUT(
 	_______, 	_______,  	_______,  	_______, 	_______,  	_______,  	_______,  	_______,  	_______,  	_______, 	_______, 	_______, 	_______, 	_______,
 	_______, 	KC_WH_U,  	KC_MS_U,  	KC_WH_D, 	_______,  	_______,  	_______,  	_______,  	_______,  	_______, 	_______, 	_______, 	_______, 	_______,
 	_______, 	KC_MS_L,  	KC_MS_D,  	KC_MS_R, 	_______,  	_______,  	_______,  	KC_BTN1,  	KC_BTN2,  	_______, 	_______, 	_______, 	         	_______,
 	_______,	_______,   	_______,   	_______,  	_______,   	_______,   	KC_ACL0,   	KC_ACL1,   	KC_ACL2,   	_______,  	_______,	_______, 	_______,    _______,
-	_______,	_______,	_______,										_______, 							_______,	_______,   	SIDE_MOD,	SIDE_VAD,    SIDE_HUI),
-
-[4] = LAYOUT(
-	_______, 	_______,  	_______,  	_______, 	_______,  	_______,  	_______,  	_______,  	_______,  	_______, 	_______, 	_______, 	_______, 	_______,
-	_______, 	_______,  	_______,  	_______, 	_______,  	_______,  	_______,  	_______,  	_______,  	_______, 	_______, 	_______, 	_______, 	_______,
-	_______, 	_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	            _______,
-	_______,    _______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	SIDE_SPD,	SIDE_SPI,  	_______,	_______,  	SIDE_VAI,    _______,
 	_______,	_______,	_______,										_______, 							_______,	_______,   	SIDE_MOD,	SIDE_VAD,    SIDE_HUI),
 
 [5] = LAYOUT(
